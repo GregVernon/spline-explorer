ca0f83c1ccb830ff3004db01ad5c340179f6043d
gvernon2 <greg.j.vernon@gmail.com>
2021-12-30 02:28:04 -0700

    Refactor tests with test creation helper function

--- SplineSpace_UnitTests.m
+++ SplineSpace_UnitTests.m
@@ -3,21 +3,20 @@ function tests = SplineSpace_UnitTests
 end
 
 function test_get_degree_list__GivenSplineSpaceReturnCorrectValue( testCase )
-    degreeList = [ 1, 1 ];
-    continuityList = [ -1, 0, -1 ];
-    lengthList = [ 1, 1 ];
-    splineSpace = SplineSpace( degreeList, continuityList, lengthList );
-    goldDegreeList = degreeList;
+    splineSpace = create_case_uniform_N2P1C0;
+    goldDegreeList = [ 1, 1 ];
     testDegreeList = splineSpace.get_degree_list();
     verifyEqual( testCase, testDegreeList, goldDegreeList );
 end
 
 function test_get_continuity_list__GivenSplineSpaceReturnCorrectValue( testCase )
-    degreeList = [ 1, 1 ];
-    continuityList = [ -1, 0, -1 ];
-    lengthList = [ 1, 1 ];
-    splineSpace = SplineSpace( degreeList, continuityList, lengthList );
-    goldContinuityList = continuityList;
+    splineSpace = create_case_uniform_N2P1C0;
+    goldContinuityList = [ -1, 0, -1 ];
     testContinuityList = splineSpace.get_continuity_list();
     verifyEqual( testCase, testContinuityList, goldContinuityList );
+end
+
+%% UTILITY FUNCTIONS
+function splineSpace = create_case_uniform_N2P1C0
+    splineSpace = SplineSpace( [ 1, 1 ], [ -1, 0, -1 ], [ 1, 1 ] );
 end
