ca679f271312cfd4888ef9098cc2625332f2b98d
gvernon2 <greg.j.vernon@gmail.com>
2021-12-30 02:22:49 -0700

    Add failing test for getting continuity list

--- SplineSpace_UnitTests.m
+++ SplineSpace_UnitTests.m
@@ -11,3 +11,13 @@ function test_get_degree_list__GivenSplineSpaceReturnCorrectValue( testCase )
     testDegreeList = splineSpace.get_degree_list();
     verifyEqual( testCase, testDegreeList, goldDegreeList );
 end
+
+function test_get_continuity_list__GivenSplineSpaceReturnCorrectValue( testCase )
+    degreeList = [ 1, 1 ];
+    continuityList = [ -1, 0, -1 ];
+    lengthList = [ 1, 1 ];
+    splineSpace = SplineSpace( degreeList, continuityList, lengthList );
+    goldContinuityList = continuityList;
+    testContinuityList = splineSpace.get_continuity_list();
+    verifyEqual( testCase, testContinuityList, goldContinuityList );
+end
