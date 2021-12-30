75082fea104a83ed17bda1b1cb510ca6cc23ede7
gvernon2 <greg.j.vernon@gmail.com>
2021-12-30 11:12:31 -0700

    Rename test functions
    MATLAB only supports max of 63 characters in function names

--- SplineSpace_UnitTests.m
+++ SplineSpace_UnitTests.m
@@ -2,28 +2,28 @@ function tests = SplineSpace_UnitTests
     tests = functiontests( localfunctions );
 end
 
-function test_get_degree_list__GivenSplineSpaceReturnCorrectValue( testCase )
+function test_get_degree_list__ReturnCorrectValue( testCase )
     splineSpace = create_case_uniform_N2P1C0;
     goldDegreeList = [ 1, 1 ];
     testDegreeList = splineSpace.get_degree_list();
     verifyEqual( testCase, testDegreeList, goldDegreeList );
 end
 
-function test_get_continuity_list__GivenSplineSpaceReturnCorrectValue( testCase )
+function test_get_continuity_list__ReturnCorrectValue( testCase )
     splineSpace = create_case_uniform_N2P1C0;
     goldContinuityList = [ -1, 0, -1 ];
     testContinuityList = splineSpace.get_continuity_list();
     verifyEqual( testCase, testContinuityList, goldContinuityList );
 end
 
-function test_get_length_list__GivenSplineSpaceReturnCorrectValue( testCase )
+function test_get_length_list__ReturnCorrectValue( testCase )
     splineSpace = create_case_uniform_N2P1C0;
     goldLengthList = [ 1, 1 ];
     testLengthList = splineSpace.get_length_list();
     verifyEqual( testCase, testLengthList, goldLengthList );
 end
 
-function test_get_element_degree__GivenSplineSpaceReturnCorrectValue( testCase )
+function test_get_element_degree__ReturnCorrectValue( testCase )
     splineSpace = create_case_uspline_N3;
     goldElementDegree = [ 1, 2, 3 ];
     for elementID = 1:length( goldElementDegree )
