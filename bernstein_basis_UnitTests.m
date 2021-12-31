c8b25c2ce11c754a93297b54390da5cd2aa084d0
gvernon2 <greg.j.vernon@gmail.com>
2021-12-30 18:43:21 -0700

    Add quadratic Bernstein basis unit test

--- bernstein_basis_UnitTests.m
+++ bernstein_basis_UnitTests.m
@@ -12,3 +12,16 @@ function test_linear_bernstein_basis__ReturnCorrectValue( testCase )
         verifyEqual( testCase, testFunction, goldFunction( basisID ) );
     end
 end
+
+function test_quadratic_bernstein_basis__ReturnCorrectValue( testCase )
+    xi = sym( "xi", "real" );
+    basisDegree = 2;
+    basisDomain = [ -1, 1 ];
+    goldFunction = [ ( ( xi - 1 ) / 2 ) ^ 2; 
+                    -( ( xi - 1 ) / 2 ) * ( xi + 1 );
+                     ( ( xi + 1 ) / 2 ) ^ 2 ];
+    for basisID = 1:3
+        testFunction = bernstein_basis( basisDegree, basisID, basisDomain );
+        verifyEqual( testCase, testFunction, goldFunction( basisID ) );
+    end
+end
