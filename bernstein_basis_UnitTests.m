3f66f9706e1814418f418c00bb110667a59380cf
gvernon2 <greg.j.vernon@gmail.com>
2021-12-30 18:36:38 -0700

    Add failing test for Bernstein basis

--- bernstein_basis_UnitTests.m
+++ bernstein_basis_UnitTests.m
@@ -3,10 +3,12 @@ function tests = bernstein_basis_UnitTests
 end
 
 function test_linear_bernstein_basis__ReturnCorrectValue( testCase )
+    xi = sym( "xi", "real" );
     basisDegree = 1;
-    basisID = 1;
     basisDomain = [ 1.5 2.0 ];
-    goldFunction = 4 - 2 * sym( "xi", "real" );
-    testFunction = bernstein_basis( basisDegree, basisID, basisDomain );
-    verifyEqual( testCase, testFunction, goldFunction );
+    goldFunction = [ 4 - 2 * xi; 2 * xi - 3];
+    for basisID = 1:2
+        testFunction = bernstein_basis( basisDegree, basisID, basisDomain );
+        verifyEqual( testCase, testFunction, goldFunction( basisID ) );
+    end
 end
