a9836ed0b7e127471777771379bc687f96fa4f30
gvernon2 <greg.j.vernon@gmail.com>
2021-12-30 18:16:41 -0700

    Fix failing test with modifications to change of basis

--- change_of_basis.m
+++ change_of_basis.m
@@ -1,3 +1,7 @@
 function mapValue = change_of_basis( referenceDomain, targetDomain )
-    mapValue = sym( "xi", "real" );
+    referenceVariate = sym( "xi", "real" );
+    A = range( targetDomain ) / range( referenceDomain );
+    b = targetDomain( 1 ) - referenceDomain( 1 );
+    y = [ [ inv(A); 0 ] [ -inv(A) * b; 1 ] ] * [ referenceVariate; 1 ];
+    mapValue = y( 1 : end-1 );
 end
