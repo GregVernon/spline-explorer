d8fdf8c219d8f94464622875b86bf09e752049a7
gvernon2 <greg.j.vernon@gmail.com>
2021-12-30 18:16:14 -0700

    Add failing test for change of basis

--- change_of_basis_UnitTests.m
+++ change_of_basis_UnitTests.m
@@ -9,3 +9,12 @@ function test_identity_ReturnCorrectValue( testCase )
     testFunction = change_of_basis( referenceDomain, targetDomain );
     verifyEqual( testCase, testFunction, goldFunction );
 end
+
+function test_translation_only_ReturnCorrectValue( testCase )
+    referenceDomain = [ 0, 1 ];
+    targetDomain = [ 1, 2 ];
+    xi = sym( "xi", "real" );
+    goldFunction =  xi - 1;
+    testFunction = change_of_basis( referenceDomain, targetDomain );
+    verifyEqual( testCase, testFunction, goldFunction );
+end
